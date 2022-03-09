aa957e25f3099c1a2d6a2c72211149249bc6ab0f
masdefry <muhammaddefryan@gmail.com>
2022-03-09 14:18:43 +0700

    Email/Phone Validator

---
+++ src/Supports/Functions/PhoneValidator.js
@@ -0,0 +1,23 @@
+// PHONE VALIDATOR
+// 1. Nomor harus diawali dengan 0
+// 2. Nomor harus memiliki panjang karakter 9-12 digit
+
+function PhoneValidator(inputUser){
+    if(inputUser[0] !== '0') return 'Nomor Harus Diawali dengan 0'
+
+    if(inputUser.length >= 9 && inputUser.length <= 12){
+        for(let i=0; i < inputUser.length; i++){
+            if(!(inputUser[i] >= 0)){
+                return 'Nomor Harus Berupa Angka'
+            }else if(inputUser[i] === ' '){
+                return 'Nomor Tanpa Spasi'
+            }
+        }
+    }else{
+        return 'Nomor Harus 9-12 Digit'
+    }
+
+    return true
+}
+
+console.log(PhoneValidator('081335719893'))
