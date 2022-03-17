b3b2152cfc88496a74797e7cb8e76f18557dcd7b
Abdi-01 <abdialghi@gmail.com>
2022-03-17 09:33:31 +0700

    register

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -38,11 +38,23 @@ class Register extends React.Component {
         }
     }
 
+    handleRegister = () => {
+        console.log(this.props.inputUsername)
+        console.log(this.props.inputEmail)
+        console.log(this.props.inPassword)
+        console.log(this.props.inConfPassword)
+
+    }
+
     render() {
         return (
-            <div className="container-fluid">
-                <div className='row mx-5 my-5 border border-grey' style={{ height: '68vh' }}>
+            <div className="container">
+                <div className='row align-items-center mx-5 my-5 border border-grey' style={{ height: '68vh' }}>
                     <div className="col-6">
+                        <img
+                            src='https://image.winudf.com/v2/image/Y29tLmVsZW5zd2VldG1vb2QxMi5mcnVpdHNuY2hvY29sYXRlX3NjcmVlbl8xXzE1Mzc4NzU1NDJfMDU1/screen-1.jpg?fakeurl=1&type=.webp'
+                            alt='portal'
+                            width="80%" />
                     </div>
                     <div className='col-6'>
                         <div className='mt-3'>
@@ -51,13 +63,26 @@ class Register extends React.Component {
                             </h2>
                         </div>
                         <div className='mt-3'>
-                            <input type="text" ref='inputEmailOrPhone' placeholder="Enter Your Email or Phone Number" className="form-control" onChange={this.onValidation} />
+                            <input type="text" ref='inputUsername' placeholder="Enter Username" className="form-control" />
                         </div>
                         <div className='mt-3'>
-                            <input type="text" placeholder="Enter Your Password" className="form-control" />
+                            <input type="text" ref='inputEmail' placeholder="Enter Your Email or Phone Number" className="form-control" onChange={this.onValidation} />
                         </div>
                         <div className='mt-3'>
-                            <input type="button" value="Register" className='btn sefruit-bg-secondary-light sefruit-main-light mb-3' />
+                            <div className='input-group'>
+                                <input type="password" ref="inPassword" placeholder="Enter Your Password" className="form-control" />
+                                <span className='input-group-text'>Show</span>
+                            </div>
+                        </div>
+                        <div className='mt-3'>
+                            <div className='input-group'>
+                                <input type="password" ref="inConfPassword" placeholder="Enter Confirmation Password" className="form-control" />
+                                <span className='input-group-text'>Show</span>
+                            </div>
+                        </div>
+                        <div className='mt-3'>
+                            {/* <input type="button" value="Register" className='btn sefruit-bg-secondary-light sefruit-main-light mb-3' /> */}
+                            <button className='btn btn-light'>Register</button>
                         </div>
                         <div>
                             <h6 className='sefruit-danger'>
