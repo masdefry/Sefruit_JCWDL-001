69e437c1e2a991b9c17cc9e19e08a1bc808a3ac4
Abdi-01 <abdialghi@gmail.com>
2022-03-17 11:51:33 +0700

    add to cart

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { API_URL } from '../Supports/helper';
+import { connect } from 'react-redux';
 
 class DetailProduct extends React.Component {
     constructor(props) {
@@ -31,11 +32,37 @@ class DetailProduct extends React.Component {
         temp += 1;
         this.setState({ qty: temp });
     }
-    
+
     handleDec = () => {
         let temp = this.state.qty;
         temp -= 1;
         this.setState({ qty: temp });
+    }
+
+    handleBuy = () => {
+        if (this.props.id) {
+            // menduplikasi data dari cart reduser, untuk mendapatkan data keranjang sebelumnya
+            let temp = [...this.props.cart];
+            // menambahkan data cart baru kedalam temp data
+            temp.push({
+                idProduct: this.state.detail.id,
+                name: this.state.detail.name,
+                image: this.state.detail.images[0],
+                qty: this.state.qty,
+                price: this.state.detail.price
+            })
+            // memperbarui data cart pada db.json setelah data cart yg baru ditambahkan
+            axios.patch(API_URL + `/users/${this.props.id}`, {
+                cart: temp
+            })
+                .then((res) => {
+                    console.log(res.data)
+                }).catch((err) => {
+                    console.log(err)
+                })
+        } else {
+            alert("Silahkan login dahulu")
+        }
     }
 
     render() {
@@ -62,14 +89,20 @@ class DetailProduct extends React.Component {
                                 <h5 className='fw-bold mt-4'>How many buy ?</h5>
                                 <div className='d-flex align-items-center'>
                                     <button className='btn btn-outline-warning'
-                                     onClick={this.handleDec}
-                                     disabled={ this.state.qty == 1 ?true:false }
-                                     >-</button>
-                                    <h5 className='mx-2'>{qty}</h5>
+                                        onClick={this.handleDec}
+                                        disabled={this.state.qty == 1 ? true : false}
+                                    >-</button>
+                                    <h5 className='mx-3'>{qty}</h5>
                                     <button className='btn btn-outline-primary' onClick={this.handleInc}>+</button>
                                 </div>
                             </div>
                         </div>
+                        <button className='mt-5 w-100 btn fw-bold'
+                            onClick={this.handleBuy}
+                            style={{ backgroundColor: "#54BAB9", color: "white" }}
+                        >
+                            Buy
+                        </button>
                     </div>
                 </div>
             </div>
@@ -77,4 +110,11 @@ class DetailProduct extends React.Component {
     }
 }
 
-export default DetailProduct;
+const mapToProps = (state) => {
+    return {
+        id: state.userReducer.id,
+        cart: state.userReducer.cart
+    }
+}
+
+export default connect(mapToProps)(DetailProduct);
